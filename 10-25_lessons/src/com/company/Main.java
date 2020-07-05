7dfc7c8e9c3abf1d70e2c55253d157671119fde0
Maria Samokhina <mariasamokhina@Marias-MacBook-Pro.local>
2020-07-05 21:35:27 +0300

    lesson #14

---
+++ 10-25_lessons/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// creating objects
+        System.out.println("Hej Hej");
+        Person vasya = new Person(120, "Vasya");
+        vasya.height=120;
+        vasya.say("Vitya");
+        System.out.println(vasya.name + "'s height is "+ vasya.height);
+        Person vlad = new Person(180);
+        System.out.println(vlad.height);
+
+
+    }
+}
